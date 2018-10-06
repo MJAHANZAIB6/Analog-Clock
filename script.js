0f1a73641a29470125aef7ba745bb55bdee4d453
mjahanzaib6 <mjahanzaib6@gmail.com>
2018-10-06 11:52:23 +0500

    complete the clock movement

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 var date = new Date();
-console.log(date);
+	console.log(date);
 
-let hr = date . getHours();
-let min = date . getMinutes();
-let sec = date . getSeconds();
-console.log("Hours:" + hr + " Minutes:" + min + " Seconds:" + sec);
+	var hr = date . getHours();
+	var min = date . getMinutes();
+	var sec = date . getSeconds();
+	console.log("Hours:" + hr + " Minutes:" + min + " Seconds:" + sec);
 
-let hrPosition = (hr * 360/12) + (min * (360/60)/12);
-let minPosition = (min * 360/60) + (sec * (360/60)/60);
-let secPosition = sec * 360/60;
+	var hrPosition = (hr * 360/12) + (min * (360/60)/12);
+	var minPosition = (min * 360/60) + (sec * (360/60)/60);
+	var secPosition = sec * 360/60;
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+function movingTheClock() {
+
+	hrPosition = hrPosition + (30/3600);
+	minPosition = minPosition + (6/60);
+	secPosition = secPosition + 6;
+
+	HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+	MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+	SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(movingTheClock, 1000);
